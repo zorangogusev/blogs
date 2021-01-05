b9b9af2038aa5164674ccdae2f7c63db9cb63886
zoran <zoran_gogusev@yahoo.com>
2021-01-05 19:11:38 +0100

    Implement dotenv and morgan

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
 
 import webRoutes from './routes/web/webRoutes.js'
 
+/** Load dotenv file witn enviromental data */
+dotenv.config({ path: './config/config.env' })
+
 const app = express()
 
-// Import Routes
+if (process.env.NODE_ENV === 'development') 
+    app.use(morgan('dev'))
+
+/** Import Routes */
 app.use('/', webRoutes)
+
 
 
 const PORT = process.env.PORT || 5000
