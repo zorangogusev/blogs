1928c78bff3f718cbbdd369fdd1afd9bf076a60f
zoran <zoran_gogusev@yahoo.com>
2021-01-06 14:24:50 +0100

    assets folder

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import colors from 'colors'
@@ -6,7 +7,7 @@ import expressLayouts from 'express-ejs-layouts'
 
 import webRoutes from './routes/webRoutes.js'
 
-/** Load dotenv file witn enviromental data */
+/** Load dotenv file with enviromental data */
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
@@ -18,6 +19,12 @@ if (process.env.NODE_ENV === 'development')
 /** Implement ejs */
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
+
+/** Load assets */
+app.use('/css', express.static(path.resolve('assets/css')))
+app.use('/js', express.static(path.resolve('assets/js')))
+app.use('/bootstrap', express.static(path.resolve('assets/bootstrap')))
+app.use('/fontawesome', express.static(path.resolve('assets/fontawesome')))
 
 /** Import Routes */
 app.use('/', webRoutes)
