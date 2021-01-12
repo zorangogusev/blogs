5e5a15c3c54fbb84b151ae625134fec096352174
zoran <zoran_gogusev@yahoo.com>
2021-01-12 17:33:34 +0100

    Dashboard pagination

--- controller/BlogerController.js
+++ controller/BlogerController.js
@@ -8,35 +8,35 @@ class BlogerController {
       * @access  Private
      */
     dashboard = async (req, res, next) => {
+        let pagination = {}
 
-        // Pagination - default is page = 1, default limit is 1
-        const page = parseInt(req.query.page, 10) || 1
-        const limit = parseInt(req.query.limit, 10) || 1
-        const startIndex = (page - 1) * limit
-        const endIndex = page * limit
-        const total = await Blog.countDocuments()
+        /** Pagination - default page = 1, default limit is 3 */
+        pagination.currentPage = parseInt(req.query.page, 10) || 1
+        const limit = parseInt(req.query.limit, 10) || 3
 
+        const totalItems = await Blog.countDocuments()
+        pagination.totalPages = Math.ceil(totalItems / limit)
+
+        /** Get blogs from database */
+        const startIndex = (pagination.currentPage - 1) * limit
+        const endIndex = pagination.currentPage * limit
         const blogs = await Blog.find({ user: req.user.id }).skip(startIndex).limit(limit)
 
-        // Pagination result
-        const pagination = {}
-
-        if (endIndex < total) {
+        if (endIndex < totalItems) {
             pagination.next = {
-                page: page + 1, 
+                nextPage: pagination.currentPage + 1, 
                 limit: limit
             }
         }
 
         if (startIndex > 0) {
             pagination.prev = {
-                page: page -1,
+                prevPage: pagination.currentPage -1,
                 limit: limit
             }
         }
-
-        // console.log('pagination is: ', pagination)
-        // console.log(typeof pagination.prev.page)
+      
+        pagination.numberOfPagesDisplay = 6
 
         res.render('bloger/dashboard', { user: req.user, blogs, pagination })
     }
